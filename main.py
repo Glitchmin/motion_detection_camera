84c13a7c87ca84bac1e188ebd47d90e3557a70a3
jchyczynski <jasiek.ch16@gmail.com>
2022-05-06 20:08:49 +0200

    simple gui

--- main.py
+++ main.py
@@ -2,13 +2,27 @@ import numpy as np
 import cv2
 from PIL import ImageGrab
 
+import kivy
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.image import Image
+from kivy.uix.layout import Layout
+from kivy.uix.widget import Widget
+from kivy.graphics.texture import Texture
 
-def motion_detector():
-    frame_count = 0
-    previous_frame = None
 
-    while True:
-        frame_count += 1
+class Detector(Image):
+    def __init__(self, fps=30, **kwargs):
+        super(Detector, self).__init__(**kwargs)
+        self.frame_count = 0
+        self.previous_frame = None
+        self.thresh = 20
+
+        Clock.schedule_interval(self.update, 1.0 / fps)
+
+    def update(self, dt):
+        self.frame_count += 1
 
         img_brg = np.array(ImageGrab.grab())
         img_rgb = cv2.cvtColor(src=img_brg, code=cv2.COLOR_BGR2RGB)
@@ -16,17 +30,17 @@ def motion_detector():
         prepared_frame = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
         prepared_frame = cv2.GaussianBlur(src=prepared_frame, ksize=(5, 5), sigmaX=0)
 
-        if previous_frame is None:
-            previous_frame = prepared_frame
-            continue
+        if self.previous_frame is None:
+            self.previous_frame = prepared_frame
+            return
 
-        diff_frame = cv2.absdiff(src1=previous_frame, src2=prepared_frame)
-        previous_frame = prepared_frame
+        diff_frame = cv2.absdiff(src1=self.previous_frame, src2=prepared_frame)
+        self.previous_frame = prepared_frame
 
         kernel = np.ones((5, 5))
         diff_frame = cv2.dilate(diff_frame, kernel, 1)
 
-        thresh_frame = cv2.threshold(src=diff_frame, thresh=20, maxval=255, type=cv2.THRESH_BINARY)[1]
+        thresh_frame = cv2.threshold(src=diff_frame, thresh=self.thresh, maxval=255, type=cv2.THRESH_BINARY)[1]
 
         contours, _ = cv2.findContours(image=thresh_frame, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
         for contour in contours:
@@ -35,10 +49,24 @@ def motion_detector():
             (x, y, w, h) = cv2.boundingRect(contour)
             cv2.rectangle(img=img_rgb, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2)
 
-        cv2.imshow('Motion detector', img_rgb)
+        frame = img_rgb
+        buf1 = cv2.flip(frame, 0)
+        buf = buf1.tostring()
+        image_texture = Texture.create(
+            size=(frame.shape[1], frame.shape[0]), colorfmt='bgr')
+        image_texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+        # display image from the texture
+        self.texture = image_texture
 
-        if cv2.waitKey(30) == 27:
-            break
+
+class DetectorWidget(BoxLayout):
+    pass
 
 
-motion_detector()
+class MotionDetectorApp(App):
+    def build(self):
+        return DetectorWidget()
+
+
+if __name__ == '__main__':
+    MotionDetectorApp().run()
