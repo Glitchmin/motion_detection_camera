69afa364b5d10eaf9ae345f95efb23573eb13b53
Adam <adam.p.kulczycki@gmail.com>
2022-05-06 21:48:29 +0200

    added gui for drawing no_detection_rectangles

--- main.py
+++ main.py
@@ -12,17 +12,22 @@ from kivy.uix.widget import Widget
 from kivy.graphics.texture import Texture
 
 
-def add_no_detection_rectangle(img_rgb, prepared_frame, previous_frame, s_x, e_x, s_y, e_y):
-    for i in range(s_x, e_x):
-        for j in range(s_y, e_y):
+def add_no_detection_rectangle(img_rgb, prepared_frame, previous_frame, rect, mult=1.0):
+    s_f, e_f = rect
+
+    s = (int(s_f[0] * mult), int(s_f[1] * mult))
+    e = (int(e_f[0] * mult), int(e_f[1] * mult))
+    print(s, e)
+    for i in range(s[0], e[0]):
+        for j in range(s[1], e[1]):
             previous_frame[i, j] = 0
             prepared_frame[i, j] = 0
-    for i in range(s_x, e_x):
-        img_rgb[i, s_y] = 100
-        img_rgb[i, e_y] = 100
-    for i in range(s_y, e_y):
-        img_rgb[s_x, i] = 100
-        img_rgb[e_x, i] = 100
+    for i in range(s[0], e[0]):
+        img_rgb[i, s[1]] = 100
+        img_rgb[i, e[1]] = 100
+    for i in range(s[1], e[1]):
+        img_rgb[s[0], i] = 100
+        img_rgb[e[0], i] = 100
 
 
 class Detector(Image):
@@ -31,11 +36,32 @@ class Detector(Image):
         self.previous_frame = None
         self.thresh = 20
         self.cam = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        self.touch_down_pos = None
+        self.rectangles = []
 
         self.source = "cam"
         self.fps = 30
 
         Clock.schedule_interval(self.update, 1.0 / self.fps)
+
+    def on_touch_down(self, touch):
+        self.touch_down_pos = self.get_click_pos(touch)
+
+    def on_touch_up(self, touch):
+        if self.touch_down_pos is not None and self.get_click_pos(touch) is not None:
+            self.rectangles.append((self.touch_down_pos, self.get_click_pos(touch)))
+
+    def get_click_pos(self, touch):
+        im_x = (self.size[0] - self.norm_image_size[0]) / 2.0 + self.x
+        im_y = (self.size[1] - self.norm_image_size[1]) / 2.0 + self.y
+        im_touch_x = touch.x - im_x
+        im_touch_y = touch.y - im_y
+        if im_touch_x < 0 or im_touch_x > self.norm_image_size[0]:
+            return None
+        elif im_touch_y < 0 or im_touch_y > self.norm_image_size[1]:
+            return None
+        else:
+            return self.norm_image_size[1] - im_touch_y, im_touch_x
 
     def update(self, dt):
         if self.source == "cam":
@@ -54,6 +80,10 @@ class Detector(Image):
         if self.previous_frame is None:
             self.previous_frame = prepared_frame
             return
+
+        for r in self.rectangles:
+            add_no_detection_rectangle(img_rgb, prepared_frame, self.previous_frame, r,
+                                       self.texture_size[0] / self.norm_image_size[0])
 
         diff_frame = cv2.absdiff(src1=self.previous_frame, src2=prepared_frame)
         self.previous_frame = prepared_frame
